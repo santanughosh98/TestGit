91c40f025330c9e854ee6da2897c9fe772c9fc5f
santanughosh98 <46629022+santanughosh98@users.noreply.github.com>
2019-01-19 00:39:30 +0530

    Update App.java
    update one test print1

--- simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
+++ simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("test print1");
     }
 
     private final String getMessage() {
